73503fb1744c2e2d4cfb3574a21737f95f77e5f3
septiannuriyanto <septian.nuryanto@gmail.com>
2023-04-06 01:16:00 +0800

    First Submission of Dicoding Final Project-Bookshelf App

---
+++ main.js
@@ -0,0 +1,219 @@
+
+const books = [];
+const RENDER_EVENT = 'render-bookshelf';
+const SAVED_EVENT = 'saved-books';
+const STORAGE_KEY = 'BOOKSHELF_APPS';
+
+function triggerIsCompleted(){
+var checkBox = document.getElementById('inputBookIsComplete');
+var buttonSubmit = document.getElementById('bookSubmit');
+if (checkBox.checked == true){
+    buttonSubmit.innerText = 'Masukkan Buku ke rak Sudah Selesai Dibaca';
+  } else {
+    buttonSubmit.innerText = 'Masukkan Buku ke rak Belum Selesai Dibaca';
+  }
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    alert('Welcome to Ohara Library')
+	const submitForm = document.getElementById('inputBook');
+	submitForm.addEventListener('submit', function (event) {
+	event.preventDefault();
+	addBook();
+	});
+
+    if (isStorageExist()) {
+        loadDataFromStorage();
+      }
+});
+
+document.addEventListener(RENDER_EVENT, function () {
+    console.log(books);
+    const completedBooksList = document.getElementById('completeBookshelfList');
+    const uncompletedBooksList = document.getElementById('incompleteBookshelfList');
+    completedBooksList.innerHTML='';
+    uncompletedBooksList.innerHTML = '';
+    for (const bookItem of books) {
+	    const bookElement = makeBook(bookItem);
+        if(bookItem.isComplete){
+            completedBooksList.append(bookElement);
+        }
+        else{
+            uncompletedBooksList.append(bookElement);
+        }
+	    
+    }
+    });
+
+    document.addEventListener('DOMContentLoaded', function () {
+        // ...
+        
+      });
+
+
+
+function addBook() {
+    // ...
+    const title = document.getElementById('inputBookTitle').value;
+	const author = document.getElementById('inputBookAuthor').value;
+    const year = document.getElementById('inputBookYear').value;
+    const checkBox = document.getElementById('inputBookIsComplete').checked
+	const generatedID = generateId();
+	const bookObject = generateBookObject(generatedID, title, author, year, checkBox);
+	books.push(bookObject);
+    document.dispatchEvent(new Event(RENDER_EVENT));
+    //.....................................................
+
+    
+   saveData();
+  }
+
+    function generateId() {
+        return +new Date();
+    }
+
+    function generateBookObject(id, title, author, year, isCompleted) {
+        return {
+            id: id,
+            title: title,
+            author: author,
+            year: year,
+            isComplete: isCompleted,
+        }
+    }
+    
+   //------------------------------------------------------------------------------------------
+   
+//    <div class="book_item">
+//    <h3>Book Title</h3>
+//    <p>Penulis: John Doe</p>
+//    <p>Tahun: 2002</p>
+
+//    <div class="action">
+//      <button class="green">Selesai dibaca</button>
+//      <button class="red">Hapus buku</button>
+//    </div>
+//  </div> 
+
+function makeBook(bookObject) {
+    const textTitle = document.createElement('h3');
+    textTitle.innerText = bookObject.title;
+   
+    const textAuthor = document.createElement('p');
+    textAuthor.innerText = `Penulis: ${bookObject.author}`;
+
+    const textYear = document.createElement('p');
+    textYear.innerText = `Tahun: ${bookObject.year}`;
+   
+    const textContainer = document.createElement('div');
+    textContainer.classList.add('inner');
+    textContainer.append(textTitle, textAuthor, textYear);
+
+    const buttonAction = document.createElement('div');
+    buttonAction.classList.add('action'); 
+    const btnGreen = document.createElement('button');
+    btnGreen.classList.add('green');
+    if(bookObject.isComplete){
+        btnGreen.innerText = "Belum Selesai dibaca";
+        btnGreen.addEventListener('click', function () {
+            removeBookFromCompleted(bookObject.id);
+            });
+    }
+    else{
+        btnGreen.innerText = "Selesai dibaca";
+        btnGreen.addEventListener('click', function () {
+            addBookToCompleted(bookObject.id);
+            });
+    }
+    
+    const btnRed = document.createElement('button');
+    btnRed.addEventListener('click', function () {
+        removeBook(bookObject.id);
+        });
+    btnRed.innerText = "Hapus Buku";
+    btnRed.classList.add('red');
+    buttonAction.append(btnGreen,btnRed)
+    const container = document.createElement('div');
+    container.classList.add('book_item');
+    container.append(textContainer);
+    container.setAttribute('id', `book-${bookObject.id}`);
+    container.append(buttonAction)
+   
+    return container;
+  }
+
+  function addBookToCompleted(bookId) {
+    // ...
+    const bookTarget = findBook(bookId);
+	if (bookTarget == null) return;
+	bookTarget.isComplete = true;
+    document.dispatchEvent(new Event(RENDER_EVENT));
+    saveData();
+  }
+   
+   
+  function removeBookFromCompleted(bookId) {
+    // ...
+    const bookTarget = findBook(bookId);
+	if (bookTarget == null) return;
+	bookTarget.isComplete = false;
+    document.dispatchEvent(new Event(RENDER_EVENT));
+    saveData();
+  }
+
+  function removeBook(bookId){
+    // ...
+    for(var i =0;i<books.length;i++){
+        if(books[i].id == bookId){
+            books.splice(i,1)
+        }
+    }
+    document.dispatchEvent(new Event(RENDER_EVENT));
+  }
+
+  function findBook(bookID) {
+	for (const bookItem of books) {
+		if (bookItem.id === bookID) {
+			return bookItem;
+		}
+	}
+	return null;
+}
+   
+   
+ //------------------------------------------------------------------
+
+  function saveData() {
+    if (isStorageExist()) {
+      const parsed = JSON.stringify(books);
+      localStorage.setItem(STORAGE_KEY, parsed);
+      document.dispatchEvent(new Event(SAVED_EVENT));
+    }
+  }
+
+
+ 
+function isStorageExist() /* boolean */ {
+  if (typeof (Storage) === undefined) {
+    alert('Browser kamu tidak mendukung local storage');
+    return false;
+  }
+  return true;
+}
+document.addEventListener(SAVED_EVENT, function () {
+    console.log(localStorage.getItem(STORAGE_KEY));
+  });
+
+function loadDataFromStorage() {
+    const serializedData = localStorage.getItem(STORAGE_KEY);
+    let data = JSON.parse(serializedData);
+   
+    if (data !== null) {
+      for (const book of data) {
+        books.push(book);
+      }
+      console.log(books);
+    }
+   
+    document.dispatchEvent(new Event(RENDER_EVENT));
+  }
---
+++ readme.md
@@ -0,0 +1,19 @@
+<h1>Simple Bookshelf App</h1>
+<p> This is my submission for final project from Dicoding Academy Course - Frontend Developer Beginner</p>
+The objective of this project is to create a bookshelf app (like a library register) which allow us to register books to the bookshelf.
+The bookshelf contains of 2 containers which is used to differentiate between books that had been read or not yet.
+<br></br>
+Basic concept used in this project is to use DOM as the primary modifier of HTML element. The way DOM changed the HTML structure is via javascript file <b>main.js</b>.
+<br></br>
+<ol>Here is some basic technique used in the javascript file:
+<li>Javascript Functions</li>
+<li>Array Operation</li>
+<li>Object Oriented Programming</li>
+<li>Event Listener</li>
+<li>Event Handler</li>
+<li>Element Selector</li>
+<li>Form</li>
+<li>Local Storage</li>
+</ol>
+
+For me, it's little confusing at first. But after several hours I figured out that this webpage has no big different with mobile apps that I've created using flutter. If you're familiar with flutter too, you might know that this web uses the same principle as flutter, like setState and sharedPreferences.
